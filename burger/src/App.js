acfae717f73d929d3a383e7653c7f99808dcef53
Ankur Sahoo <shanky442@gmail.com>
2020-05-25 23:20:22 +0100

    react routing

--- burger/src/App.js
+++ burger/src/App.js
@@ -3,11 +3,16 @@ import React from 'react';
 import './App.css';
 import Layout from './components/Layout/Layout';
 import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder';
+import Checkout from '../src/containers/Checkout/Checkout';
+import { Route, Switch } from 'react-router-dom';
 
 function App() {
   return (
     <Layout>
-      <BurgerBuilder/>
+      <Switch>
+        <Route path="/checkout" component={Checkout}/>
+        <Route path="/" exact component={BurgerBuilder}/>        
+      </Switch>      
     </Layout>
   );
 }
